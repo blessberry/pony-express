002f3481d66bf4123434e89eb3f6fcbc8161cb5e
blessberry <beraka.emmanuel@protonmail.ch>
2020-10-10 12:38:59 +0000

    Middlewares set up

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const unHandledRouter = require('./routes/unhandled');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/users',usersRouter);
 app.use('/emails', emailsRouter);
 app.use(unHandledRouter);
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 const getUsers = (req, res) => res.send(users);
 const getUser = (req, res) => res.send(users.find(user => user.id===req.params.id));
+const postUser = (req, res) => {users.push(req.body); res.status(201).send(req.body)};
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
+router.post('/', postUser);
 
 module.exports = router;
